1bb8070236eaa0c4b49d3eba6e5a79ef0d9e1484
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-12 22:45:15 -0400

    commit tree 23ba8e91e210eac89267c90ce30672886dc397cd

--- index.js
+++ index.js
@@ -4,15 +4,34 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if (Array.isArray(collection)) {
+        // collection.forEach(element => callback(element));
+        for (let i = 0; i < collection.length; i++) {
+          callback.call(collection[i], i, collection);
+        }
+      } else {
+        const entries = Object.entries(collection);
+        for (let i = 0; i < entries.length; i++) {
+          callback.call(entries[i][1], entries[i][0], collection);
+        }
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      if (Array.isArray(collection)) {
+        return collection.map(element => callback(element));
+      } else {
+        const result = [];
+        for (let key in collection) {
+          result.push(callback(collection[key]));
+        }
+        return result;
+      }
     },
 
-    reduce: function() {
+    reduce: function(collection, callback, acc) {
 
     },
 
